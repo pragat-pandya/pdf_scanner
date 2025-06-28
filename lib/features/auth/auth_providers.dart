aa2fe772f2d8e4cbde8ddd18d172ddfc146025c6
Pragat Pandya <pragat.pandya@gmail.com>
2025-06-28 18:10:10 +0530

    Define User Provider a stateProvider.

--- lib/features/auth/auth_providers.dart
+++ lib/features/auth/auth_providers.dart
@@ -2,8 +2,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pdf_scanner/core/providers/firebase_providers.dart';
 import 'package:pdf_scanner/features/auth/contoller/auth_controller.dart';
 import 'package:pdf_scanner/features/auth/repository/auth_repository.dart';
+import 'package:pdf_scanner/models/user_model.dart';
 
 /// Providers for authentication features in the PDF Scanner app.
+
+final userProvider = StateProvider<UserModel?>((ref) => null);
 
 /// Provides an instance of [AuthRepository] that handles authentication operations.
 /// It uses Firebase Firestore, Firebase Auth, and Google Sign-In.
@@ -17,6 +20,8 @@ final authRepositoryProvider = Provider((ref) => AuthRepository(
 /// It uses the [AuthRepository] to perform sign-in operations.
 final authControllerProvider = Provider(
     (ref) => AuthController(authRepository: ref.watch(authRepositoryProvider)));
+
+
 
 
 
