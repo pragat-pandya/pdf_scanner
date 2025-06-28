4cdc64b32b9a8e4f2af5fe39028a599658ab17de
Pragat Pandya <pragat.pandya@gmail.com>
2025-06-28 14:32:22 +0530

    Define useModel.

---
+++ lib/models/user_model.dart
@@ -0,0 +1,119 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+
+class UserModel {
+  final String name;
+  final String email;
+  final String profilePic;
+  final String banner;
+  final String uid;
+  final bool isAuthenticated;
+  final bool isPremium;
+  final int coins;
+  final List<String> awards;
+  UserModel({
+    required this.name,
+    required this.email,
+    required this.profilePic,
+    required this.banner,
+    required this.uid,
+    required this.isAuthenticated,
+    required this.isPremium,
+    required this.coins,
+    required this.awards,
+  });
+
+  UserModel copyWith({
+    String? name,
+    String? email,
+    String? profilePic,
+    String? banner,
+    String? uid,
+    bool? isAuthenticated,
+    bool? isPremium,
+    int? coins,
+    List<String>? awards,
+  }) {
+    return UserModel(
+      name: name ?? this.name,
+      email: email ?? this.email,
+      profilePic: profilePic ?? this.profilePic,
+      banner: banner ?? this.banner,
+      uid: uid ?? this.uid,
+      isAuthenticated: isAuthenticated ?? this.isAuthenticated,
+      isPremium: isPremium ?? this.isPremium,
+      coins: coins ?? this.coins,
+      awards: awards ?? this.awards,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'email': email,
+      'profilePic': profilePic,
+      'banner': banner,
+      'uid': uid,
+      'isAuthenticated': isAuthenticated,
+      'isPremium': isPremium,
+      'coins': coins,
+      'awards': awards,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      name: map['name'] as String,
+      email: map['email'] as String,
+      profilePic: map['profilePic'] as String,
+      banner: map['banner'] as String,
+      uid: map['uid'] as String,
+      isAuthenticated: map['isAuthenticated'] as bool,
+      isPremium: map['isPremium'] as bool,
+      coins: map['coins'] as int,
+      awards: List<String>.from(
+        (map['awards'] as List<String>),
+      ),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'UserModel(name: $name, email: $email, profilePic: $profilePic, banner: $banner, uid: $uid, isAuthenticated: $isAuthenticated, isPremium: $isPremium, coins: $coins, awards: $awards)';
+  }
+
+  @override
+  bool operator ==(covariant UserModel other) {
+    if (identical(this, other)) return true;
+
+    return other.name == name &&
+        other.email == email &&
+        other.profilePic == profilePic &&
+        other.banner == banner &&
+        other.uid == uid &&
+        other.isAuthenticated == isAuthenticated &&
+        other.isPremium == isPremium &&
+        other.coins == coins &&
+        listEquals(other.awards, awards);
+  }
+
+  @override
+  int get hashCode {
+    return name.hashCode ^
+        email.hashCode ^
+        profilePic.hashCode ^
+        banner.hashCode ^
+        uid.hashCode ^
+        isAuthenticated.hashCode ^
+        isPremium.hashCode ^
+        coins.hashCode ^
+        awards.hashCode;
+  }
+}
