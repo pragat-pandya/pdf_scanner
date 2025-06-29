242714a7d0fc2f21ecce19f0bfdf1cc11fe7e710
Pragat Pandya <pragat.pandya@gmail.com>
2025-06-29 20:14:48 +0530

    Add HomeScreen AppBar UI.

--- lib/features/auth/home/screens/home_screen.dart
+++ lib/features/auth/home/screens/home_screen.dart
@@ -9,6 +9,26 @@ class HomeScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider)!;
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: false,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.menu),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            icon: CircleAvatar(
+              backgroundImage: NetworkImage(user.profilePic),
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
       body: Center(
         child: Text(
           user.uid,
